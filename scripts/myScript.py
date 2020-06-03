2901e9088f24adc1771fb0e71e59f8f59be5f661
Ayomide100 <fasakinoluwaseyi4@gmail.com>
2020-06-03 17:50:25 +0100

    first commit

---
+++ scripts/myScript.py
@@ -0,0 +1,4 @@
+name = "Fasakin Oluwaseyi Ayomide"
+hng_id= "HNG-03081"
+email ="royaltechpriest@gmail.com"
+print("Hello World, this is "+name +"with HNGi7 ID" + hng_id + " using python for stage 2 task")
